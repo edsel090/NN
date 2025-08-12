b12a1842b9ff81be7f7d258aa6bc00edb7d4681a
EdselHernandez <edselh702@gmail.com>
2025-08-11 22:39:30 -0700

    test if math is consistent

--- main.cpp
+++ main.cpp
@@ -8,5 +8,16 @@
 using namespace std;
 
 int main(){
+    srand(42); // Seed for random number generation
+    Neuron neuron;
+    vector<double> weights = {1.0,2.0,3.0}; // Example weights
+    double test_bias = 0.5; // Example bias
+    neuron.set_weights(weights);
+    neuron.set_bias(test_bias);
+    vector<double> inputs = {0.5, -0.2, 0.1};
+    double output = neuron.forward(inputs);
+
+    cout << "Output of the neuron: " << output << endl;
+    return 0;
 
 }
